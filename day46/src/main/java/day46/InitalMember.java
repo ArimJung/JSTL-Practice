ac2800eec01fa50df4e54cedffa015d108cf320d
USER <USER@DESKTOP-9273RIA>
2022-08-17 11:00:15 +0900

    JSTL practice

---
+++ day46/src/main/java/day46/InitalMember.java
@@ -0,0 +1,54 @@
+package day46;
+
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class InitalMember
+ *
+ */
+@WebListener
+public class InitalMember implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public InitalMember() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // 톰캣의 종료
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // context == application scope 
+    	// 톰캣(서버,서블릿 컨테이너)의 시작을 감지(모니터링)
+    	
+    	ArrayList<Member> datas = new ArrayList<Member>();
+    	
+    	for(int i=1; i<10; i++) {
+    		Member data= new Member("홍길동"+i, "010-1234-567"+i);
+    		datas.add(data);
+    	}
+    	datas.add(new Member("임꺽정",null));
+    	datas.add(new Member("아무무",null));
+    	
+    	// context == application scope
+    	ServletContext context = sce.getServletContext();
+    	context.setAttribute("datas", datas);
+    	context.setAttribute("data", new Member());
+    
+    
+    }
+}
